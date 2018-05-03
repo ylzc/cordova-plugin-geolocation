6c036f5f4b3662fc08199a2158fb92d35f60745b
yel <yel@sucsoft.com>
2018-05-03 19:01:10 +0800

    0.3.6

--- www/Position.js
+++ www/Position.js
@@ -21,13 +21,13 @@
 
 var Coordinates = require('./Coordinates');
 
-var Position = function (coords, timestamp) {
+var Position = function(coords, timestamp) {
     if (coords) {
         this.coords = new Coordinates(coords.latitude, coords.longitude, coords.altitude, coords.accuracy, coords.heading, coords.velocity, coords.altitudeAccuracy);
     } else {
         this.coords = new Coordinates();
     }
-    this.timestamp = (timestamp !== undefined) ? timestamp : new Date().getTime();
+    this.timestamp = (timestamp !== undefined) ? timestamp : new Date();
 };
 
 module.exports = Position;
--- www/PositionError.js
+++ www/PositionError.js
@@ -26,13 +26,13 @@
  * @param code
  * @param message
  */
-var PositionError = function (code, message) {
+var PositionError = function(code, message) {
     this.code = code || null;
     this.message = message || '';
 };
 
-PositionError.prototype.PERMISSION_DENIED = PositionError.PERMISSION_DENIED = 1;
-PositionError.prototype.POSITION_UNAVAILABLE = PositionError.POSITION_UNAVAILABLE = 2;
-PositionError.prototype.TIMEOUT = PositionError.TIMEOUT = 3;
+PositionError.PERMISSION_DENIED = 1;
+PositionError.POSITION_UNAVAILABLE = 2;
+PositionError.TIMEOUT = 3;
 
 module.exports = PositionError;
